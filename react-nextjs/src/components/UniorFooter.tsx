8f75e64cbb601396a275bcc154080ce5acd786f3
Berigny <134203992+Berigny@users.noreply.github.com>
2023-10-13 04:41:41 +0000

    Update

--- react-nextjs/src/components/UniorFooter.tsx
+++ react-nextjs/src/components/UniorFooter.tsx
@@ -1,13 +1,15 @@
 const Footer: React.FC = () => {
     return (
-      <footer>
-        <hr className="mt-5 mb-4" />
-        <p className="text-muted">
-          Created and open sourced by the Bootstrap team. Licensed MIT.
-        </p>
-      </footer>
+        <footer>
+            <hr className="mt-5 mb-4" />
+            <p className="text-muted">
+                Created and open sourced by the Bootstrap team. Licensed MIT.
+            </p>
+            <p className="text-muted">
+                &copy; {new Date().getFullYear()} Unior. Created by realtheme.
+            </p>
+        </footer>
     );
-  };
+};
   
-  export default Footer;
-  
+export default Footer;
