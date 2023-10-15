6001aa9269b3a9c8227848cc56aabddeaba4115f
Berigny <134203992+Berigny@users.noreply.github.com>
2023-10-15 04:43:33 +0000

    Update

--- react-nextjs/src/components/UniorHeader.tsx
+++ react-nextjs/src/components/UniorHeader.tsx
@@ -1,35 +1,37 @@
 import Link from "next/link";
+import userImg from './path-to/assets/img/avatars/user.png'; // Update the path
 
 const Header: React.FC = () => {
   return (
-    <header className="d-flex justify-content-between align-items-md-center pb-3 mb-5 border-bottom">
-      <h1 className="h4">
-        <Link
-          href="/"
-          className="d-flex align-items-center text-dark text-decoration-none"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            className="bi bi-bootstrap-fill d-inline-block me-2"
-            viewBox="0 0 16 16"
-          >
-            <path d="M6.375 7.125V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375v2.725z" />
-            <path d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4h-8zm1.06 12V3.545h3.399c1.587 0 2.543.809 2.543 2.11 0 .884-.65 1.675-1.483 1.816v.1c1.143.117 1.904.931 1.904 2.033 0 1.488-1.084 2.396-2.888 2.396H5.062z" />
-          </svg>
-          <span>React</span>
-        </Link>
-      </h1>
-      <a
-        href="https://github.com/twbs/examples/tree/main/react-nextjs/"
-        target="_blank"
-        rel="noopener"
-      >
-        View on GitHub
-      </a>
-    </header>
+    <div className="wrapper bg-noise">
+      {/* Navigation :: Start */}
+      <div className="navigation hide-scrollbar bg-noise py-4" data-bs-theme="dark">
+        {/* ... other navigation items ... */}
+        {/* Card :: Start */}
+        <div className="card card-upgrade bg-quaternary border-0 mx-3 mt-auto">
+          <div className="card-body p-3">
+            <div className="d-flex justify-content-between">
+              <div className="d-flex align-items-center">
+                <div className="avatar avatar-sm">
+                  <img src={userImg} alt="" />
+                </div>
+                <div className="ms-2">
+                  <h6 className="mb-0">realtheme</h6>
+                  <p className="text-body-secondary fs-12 mb-0">@realtheme</p>
+                </div>
+              </div>
+              <div className="ms-2">
+                <span className="badge bg-success fs-14">Free</span>
+              </div>
+            </div>
+            <a href="plans.html" className="btn btn-sm btn-outline-primary w-100 text-white mt-3" role="button">View Plans</a>
+          </div>
+        </div>
+        {/* Card :: End */}
+      </div>
+      {/* Navigation :: End */}
+      {/* ... other components ... */}
+    </div>
   );
 };
 
