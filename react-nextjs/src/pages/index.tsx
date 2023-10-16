052d2fbdd869ca0fc69585e7e93f709f45c36904
Berigny <134203992+Berigny@users.noreply.github.com>
2023-10-16 10:46:05 +0000

    Update

--- react-nextjs/src/pages/index.tsx
+++ react-nextjs/src/pages/index.tsx
@@ -11,11 +11,11 @@ export default function Home() {
   return (
     <>
       <UniorHead title="Home Page" />
-      <Container as="div" className="wrapper bg-noise">
+      <div className="wrapper bg-noise">
         <UniorNavigation />
         <Container as="div" className="nav-backdrop"></Container>
         <UniorMain />
-      </Container>
+      </div>
       <UniorSearchModal />
       <UniorSettingsModal />
       <UniorFooter />
