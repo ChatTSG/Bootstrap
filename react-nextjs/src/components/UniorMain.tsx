22c1dfce1e3d0d26e1e40b8c6fcc2fcc3255ed0d
Berigny <134203992+Berigny@users.noreply.github.com>
2023-10-16 10:08:51 +0000

    Update

--- react-nextjs/src/components/UniorMain.tsx
+++ react-nextjs/src/components/UniorMain.tsx
@@ -17,8 +17,23 @@ const MainComponent = () => {
           </div>
           {/* Toolbar Right */}
           <div className="d-flex align-items-center">
-            {/* Additional toolbar content... */}
-          </div>
+            <button className="btn btn-icon btn-sm me-2" type="button" title="Notifications">
+              <i className="ri-notification-2-line"></i>
+            </button>
+            <button className="btn btn-icon btn-sm js-toggle-sidebar" type="button" title="Toggle Sidebar">
+              <i className="ri-layout-right-line"></i>
+            </button>
+            <div className="avatar avatar-sm mx-2">
+              <img src={userImg} alt="Avatar" />
+            </div>
+            <div className="row align-items-center g-1 ms-1">
+              <div className="col-auto">
+                <div className="form-check form-switch form-switch-theme">
+                  <input className="form-check-input" type="checkbox" role="switch" id="switchTheme" title="Theme Switcher" value="light" />
+                </div>
+              </div>
+            </div>
+        </div>
         </div>
         <div className="main-content">
           <div className="main-body">
